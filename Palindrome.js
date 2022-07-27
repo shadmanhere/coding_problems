56f642114746fcbfb0d12dcf3cd5ec695da91672
Shadman Ali <shadman.ali@live.co.uk>
2022-07-27 16:59:15 +0530

    ispPalindrome

---
+++ Palindrome.js
@@ -0,0 +1,18 @@
+// Question 2:Palindrome-You are given a string. Write a function to check whether the string is a palindrome or not.
+
+function isPalindrome(string) {
+    for(let i = 0; i < string.length; i++) {
+        if(string[i] !== string[string.length - 1 - i]){
+            return false;
+        }
+        if(i === string.length - 1 - i) break;
+    }
+    return true
+}
+
+console.log(
+    isPalindrome("okay"),
+    isPalindrome("malayalam"),
+    isPalindrome("ab"),
+    isPalindrome("a"),
+)
