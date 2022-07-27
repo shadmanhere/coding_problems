7af66d6823ac37a73b60f88e0d4471506b353348
Shadman Ali <shadman.ali@live.co.uk>
2022-07-27 14:12:17 +0530

    Non repeating character

---
+++ NonRepeatingCharacter.js
@@ -0,0 +1,21 @@
+// Question 1:Non repeating character - You are given a string consisting of only lower case and upper-case English Alphabets and integers 0 to 9. Write a function that will take this string as Input and return the index of the first character that is non-repeating.
+
+function findNonRepeatingCharacter(string){
+    let repeat;
+    for(let i=0;i<string.length;i++){
+        repeat = false;
+        for(let j=0;j<string.length;j++){
+            if(string[i]===string[j] && i!==j){
+                repeat = true;
+                break;
+            }
+        }
+        if(!repeat){
+            return i;
+        }
+    }
+    return null;
+}
+console.log(
+    findNonRepeatingCharacter('aabbcc')
+)
