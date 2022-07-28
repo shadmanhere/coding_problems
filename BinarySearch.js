fb63b5fb03a81ec49c483e2b273d00ce21616a61
Shadman Ali <shadman.ali@live.co.uk>
2022-07-28 10:02:56 +0530

    Binary Search

--- BinarySearch.js
+++ BinarySearch.js
@@ -1,19 +1,43 @@
-const binarySearch = function(search,value) {
+// Question 1:Binary Search - Given an array of integers which is sorted in ascending order, and a target integer, write a function to search for whether the target integer is there in the given array. If it is there then return its index. Otherwise, return -1. You must write an algorithm with O(log n) runtime complexity
+
+// Iterative
+const binarySearchIterative = function(nums,target) {
     let left = 0;
-    let right = search.length - 1;
+    let right = nums.length - 1;
     let middle;
     while(left <= right){
         middle = Math.floor((left+right)/2)
-        if(value < search[middle]) right = middle - 1;
-        else if(value > search[middle]) left = middle + 1;
+        if(target < nums[middle]) right = middle - 1;
+        else if(target > nums[middle]) left = middle + 1;
         else return middle;
     }
-    return null
+    return -1
 }
 
 console.log(
-    binarySearch([1,2,3,4,5],5),
-    binarySearch([1,2,3,4,5],6),
-    binarySearch([1,2,3,4,5],2),
-    binarySearch([1,2,3,4,5],1),
+    binarySearchIterative([1,2,3,4,5],5),
+    binarySearchIterative([1,2,3,4,5],6),
+    binarySearchIterative([1,2,3,4,5],2),
+    binarySearchIterative([1,2,3,4,5],1),
+)
+
+
+// Recursive
+const binarySearchRecursive = function(nums,target){
+    const helper = function(nums,target,left,right){
+        // base case
+        if(left > right) return -1;
+        const middle = Math.floor((left+right)/2)
+        if(target === nums[middle]) return middle;
+        else if(target > nums[middle]) return helper(nums,target,middle+1,right) //only value of left changed 
+        else return helper(nums,target,left,middle-1) // only value of right changed
+    }
+    return helper(nums,target,0,nums.length-1)
+}
+
+console.log(
+    binarySearchRecursive([1,2,3,4,5],5),
+    binarySearchRecursive([1,2,3,4,5],6),
+    binarySearchRecursive([1,2,3,4,5],2),
+    binarySearchRecursive([1,2,3,4,5],1),
 )
