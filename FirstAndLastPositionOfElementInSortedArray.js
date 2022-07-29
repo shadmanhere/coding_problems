b7df2e3bfdd0c50db2c5a5edb367adcbd945c9bf
Shadman Ali <shadman.ali@live.co.uk>
2022-07-29 17:01:20 +0530

    Find First and Last Position of Element in Sorted Array

--- FirstAndLastPositionOfElementInSortedArray.js
+++ FirstAndLastPositionOfElementInSortedArray.js
@@ -1,5 +1,7 @@
 // Question 1:Find First and Last Position of Element in Sorted Array-You are given an array of integers sorted in non-decreasing order, find the starting and ending position of a given target value. If target is not found in the array, return [-1, -1]. You must write an algorithm with O(log n) runtime complexity.
- const search = function (array,target) {
+ 
+// Iterative
+const search = function (array,target) {
     const leftExtreme = findLeftExtreme(array,target);
     const rightExtreme = findRightExtreme(array,target);
     return [leftExtreme, rightExtreme];
@@ -46,4 +48,47 @@
  console.log(
     search([1,2,3,4,5,5,5,5,6,7],5),
     search([1,2,3,3,3,3,3,4,5,6,7],3)
+ )
+
+ // Recursive
+ const searchForRangeRec = function (array,target) {
+    const range = [-1,-1]
+    findLeftExtremeRec(array,target,range);
+    findRightExtremeRec(array,target,range);
+    return range;
+ }
+
+ const findLeftExtremeRec = function (array,target,range, left=0, right=array.length - 1) {
+    //base case
+    if(left>right) return;
+    const middle = Math.floor((left+right)/2)
+    if(array[middle]===target){
+        if(middle===0) range[0]=0;
+        else if(array[middle - 1]===target){
+            findLeftExtremeRec(array,target,range,left,middle-1);
+        } else range[0] = middle;
+    } else if(target< array[middle]){
+        findLeftExtreme(array,target,range,left,middle-1);
+    } else findRightExtreme(array,target,range,middle+1,right);
+ }
+
+ const findRightExtremeRec = function (array,target,range,left=0,right=array.length-1){
+    // base case
+    if(left>right) return;
+    const middle = Math.floor((left+right)/2);
+    if(array[middle] === target){
+        if(middle===array.length) range[1] = middle;
+        else if(target===array[middle+1]){
+            findRightExtremeRec(array,target,range,middle+1,right);
+        } else range[1] = middle;
+    } else if(target< array[middle]){
+        findRightExtremeRec(array,target,range,left,middle-1);
+    } else {
+        findRightExtremeRec(array,target,range,middle+1,right);
+    }
+ }
+
+ console.log(
+    searchForRangeRec([1,2,3,4,5,5,5,5,6,7],5),
+    searchForRangeRec([1,2,3,3,3,3,3,4,5,6,7],3)
  )
